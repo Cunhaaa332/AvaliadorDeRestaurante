df286e7b980d4ab9ec412457986c41bf87cc9ca7
Cunhaaa <gabriel.dsilveira@al.infnet.edu.br>
2021-03-29 22:12:21 -0300

    Layout das telas de login e cadastro.

--- app/src/main/java/com/infnet/avaliadorderestaurantes/ui/avaliacao/form/FormAvaliacaoViewModel.kt
+++ app/src/main/java/com/infnet/avaliadorderestaurantes/ui/avaliacao/form/FormAvaliacaoViewModel.kt
@@ -1,7 +1,21 @@
 package com.infnet.avaliadorderestaurantes.ui.avaliacao.form
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.infnet.avaliadorderestaurantes.database.AvaliacaoDao
 
-class FormAvaliacaoViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class FormAvaliacaoViewModel(private val avaliacaoDao: AvaliacaoDao) : ViewModel() {
+    private val _status = MutableLiveData<Boolean>()
+    val status: LiveData<Boolean> = _status
+
+    private val _message = MutableLiveData<String?>()
+    val message: LiveData<String?> = _message
+
+    init {
+        _status.value = false
+        _message.value = null
+    }
+
+
 }
